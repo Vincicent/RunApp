5a925aa73ba19c2ed9c62ff1134c87bad34feaef
Vincicent <Prive@.com>
2024-05-17 12:10:20 +0200

    feat(core) setup session storage

---
+++ core/data/src/main/java/com/vincicent/core/data/auth/AuthInfoSerializable.kt
@@ -0,0 +1,10 @@
+package com.vincicent.core.data.auth
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AuthInfoSerializable(
+    val accessToken: String,
+    val refreshToken: String,
+    val userId: String
+)
--- core/data/src/main/java/com/vincicent/core/data/di/CoreDataModule.kt
+++ core/data/src/main/java/com/vincicent/core/data/di/CoreDataModule.kt
@@ -1,10 +1,15 @@
 package com.vincicent.core.data.di
 
+import com.vincicent.core.data.auth.EncryptedSessionStorage
 import com.vincicent.core.data.networking.HttpClientFactory
+import com.vincicent.core.domain.SessionStorage
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreDataModule = module {
     single {
         HttpClientFactory().build()
     }
+    singleOf(::EncryptedSessionStorage).bind<SessionStorage>()
 }
