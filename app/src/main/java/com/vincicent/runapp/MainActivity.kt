99ea260ca4bb3885ea62412735e9e72efccc567d
Vincicent <Prive@.com>
2024-05-20 14:46:35 +0200

    feat(app) create splash and check if user logged in

--- app/src/main/java/com/vincicent/runapp/MainActivity.kt
+++ app/src/main/java/com/vincicent/runapp/MainActivity.kt
@@ -4,21 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
-import com.vincicent.core.presentation.designsystem.AnalyticsIcon
 import com.vincicent.core.presentation.designsystem.RunAppTheme
 import com.vincicent.runapp.navigation.NavigationRoot
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModel<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.state.isCheckingAuth
+            }
+        }
         setContent {
             RunAppTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,8 +30,14 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val navController = rememberNavController()
-                    NavigationRoot(navController = navController)
+
+                    if (!viewModel.state.isCheckingAuth) {
+                        val navController = rememberNavController()
+                        NavigationRoot(
+                            navController = navController,
+                            isLoggedIn = viewModel.state.isLoggedIn
+                        )
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/vincicent/runapp/MainState.kt
@@ -0,0 +1,6 @@
+package com.vincicent.runapp
+
+data class MainState(
+    val isLoggedIn: Boolean = false,
+    val isCheckingAuth: Boolean = false
+)
