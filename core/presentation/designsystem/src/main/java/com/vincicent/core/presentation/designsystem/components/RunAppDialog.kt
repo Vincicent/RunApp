b8ee989de2da102f3096f680eb7f6da3fa87f577
Vincicent <Prive@.com>
2024-05-22 12:03:35 +0200

    feat(designsystem) create RunAppDialog

---
+++ core/presentation/designsystem/src/main/java/com/vincicent/core/presentation/designsystem/components/RunAppDialog.kt
@@ -0,0 +1,78 @@
+package com.vincicent.core.presentation.designsystem.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.vincicent.core.presentation.designsystem.RunAppTheme
+
+@Composable
+fun RunAppDialog(
+    title: String,
+    onDismiss: () -> Unit,
+    description: String,
+    primaryButton: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    secondaryButton: @Composable () -> Unit = {}
+) {
+    Dialog(onDismissRequest = onDismiss) {
+        Column(
+            modifier = modifier
+                .clip(RoundedCornerShape(15.dp))
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(15.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = title,
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Text(
+                text = description,
+                fontSize = 12.sp,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                secondaryButton()
+                primaryButton()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun RunAppDialogPreview() {
+    RunAppTheme {
+        RunAppDialog(
+            title = "test",
+            onDismiss = {},
+            description = "This is a test",
+            primaryButton = {}
+        )
+    }
+}
